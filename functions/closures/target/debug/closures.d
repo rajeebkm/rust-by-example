784cd779bea5ec7a16b251652c41e68b9b2d7647
rajeebkm <rkm2541997@gmail.com>
2022-05-16 14:54:27 +0530

    Functions Closures added

---
+++ functions/closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/home/hp/RAJEEB/Rajeeb/Rust/Rust_Projects/Rust\ By\ Example/functions/closures/target/debug/closures: /home/hp/RAJEEB/Rajeeb/Rust/Rust_Projects/Rust\ By\ Example/functions/closures/src/main.rs
---
+++ functions/closures/target/debug/deps/closures-527cfaacad74fe69.d
@@ -0,0 +1,5 @@
+/home/hp/RAJEEB/Rajeeb/Rust/Rust_Projects/Rust By Example/functions/closures/target/debug/deps/closures-527cfaacad74fe69: src/main.rs
+
+/home/hp/RAJEEB/Rajeeb/Rust/Rust_Projects/Rust By Example/functions/closures/target/debug/deps/closures-527cfaacad74fe69.d: src/main.rs
+
+src/main.rs:
